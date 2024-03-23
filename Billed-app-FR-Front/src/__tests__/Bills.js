7a1c8c10541f4478503e00972519347545f9c6b9
Annabelle <annabelle.braye@gmail.com>
2024-03-23 09:19:00 +0100

    test(bills.js): Add test when loading is true and when there is an error.

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -2,18 +2,36 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import {screen, waitFor, fireEvent} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
+import Bills from "../containers/Bills.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+import userEvent from '@testing-library/user-event'
+import mockStore from "../__mocks__/store"
 
 import router from "../app/Router.js";
 
+jest.mock("../app/store", () => mockStore)
+
 describe("Given I am connected as an employee", () => {
+  describe("When loading is true", () => {
+    it("Should render loading page", () => {
+      document.body.innerHTML = BillsUI({loading: true})
+      expect(screen.getAllByText('Loading...')).toBeTruthy()
+    })
+  })
+
+  describe("When error is true", () => {
+    it("Should render error page", () => {
+      document.body.innerHTML = BillsUI({error: 'There is an error'})
+      expect(screen.getAllByText('Erreur')).toBeTruthy()
+    })
+  })
+
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
@@ -38,5 +56,34 @@ describe("Given I am connected as an employee", () => {
       const sortedDates = [...dates].sort(antiChrono)
       expect(sortedDates).toEqual(dates)
     })
+
+    // describe("When I click on first bill eye-icon", async () => {
+    //   it("Should open modal", async () => {
+    //     Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    //     window.localStorage.setItem('user', JSON.stringify({
+    //       type: 'Employee'
+    //     }))
+    //     document.body.innerHTML = BillsUI(bills)
+    //     const onNavigate = (pathname) => {
+    //       document.body.innerHTML = ROUTES({ pathname })
+    //     }
+    //     const store = null
+    //     const bill = new Bills({
+    //       document, onNavigate, store, localStorage: window.localStorage
+    //     })
+
+    //     const handleClickIconEye = jest.fn(bill.handleClickIconEye)
+    //     await waitFor(() => {
+    //       screen.getAllByTestId('icon-eye')
+    //     })
+    //     const eye = screen.getAllByTestId('icon-eye')
+    //     eye.addEventListener('click', handleClickIconEye)
+    //     userEvent.click(eye)
+    //     expect(handleClickIconEye).toHaveBeenCalled()
+
+    //     const modale = screen.getByTestId('modaleFile')
+    //     expect(modale).toBeTruthy()
+    //   })
+    // })
   })
 })
