eb7ce7622cba452ebd2acd4378cba6c8c7a13430
Annabelle <annabelle.braye@gmail.com>
2024-04-04 16:49:59 +0200

    test(bills): Add get test.

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -12,7 +12,6 @@ import {localStorageMock} from "../__mocks__/localStorage.js";
 import mockStore from "../__mocks__/store"
 
 import router from "../app/Router.js";
-import Router from "../app/Router.js"
 
 jest.mock("../app/store", () => mockStore)
 
@@ -138,5 +137,51 @@ describe("Given I am connected as an employee", () => {
         expect(screen.getByText('Envoyer une note de frais')).toBeTruthy()
       })
     })
+  })
+})
+
+// test d'intégration GET
+describe("Given I'm a user connected as Employee", () => {
+  describe("When I navigate to Bills", () => {
+    describe("When an error occurs on API", () => {
+      beforeEach(() => {
+        jest.spyOn(mockStore, "bills")
+        Object.defineProperty(window,'localStorage',{ value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee',
+        }))
+        const root = document.createElement("div")
+        root.setAttribute("id", "root")
+        document.body.append(root)
+        router()
+      })
+
+      it("should fetch bills from an API and fails with 404 message error", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list : () =>  {
+              return Promise.reject(new Error("Erreur 404"))
+            }
+          }})
+        window.onNavigate(ROUTES_PATH.Bills)
+        await new Promise(process.nextTick);
+        const message = await screen.getByText(/Erreur 404/)
+        expect(message).toBeTruthy()
+      })
+
+      it("should fetch messages from an API and fails with 500 message error", async () => {
+        mockStore.bills.mockImplementationOnce(() => {
+          return {
+            list : () =>  {
+              return Promise.reject(new Error("Erreur 500"))
+            }
+          }})
+
+        window.onNavigate(ROUTES_PATH.Bills)
+        await waitFor(() => screen.getByText(/Erreur 500/))
+        expect(screen.getByText(/Erreur 500/)).toBeTruthy()
+      })
+    })
+
   })
 })
